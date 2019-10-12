2a449cceace88b0727bcdc93c4c84c7ae0b81ec2
Maksim Koltsov <tirinox@gmail.com>
2019-10-12 11:31:54 +0300

    migrated to aiogram, which is able to recieve /start code

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # TooHeavyBot
 Telegram bot to control you weight
+
+Currently deployed at
+
+https://telegram.me/TooHeavyBot
+
+Link to send codes (dev):
+
+https://telegram.me/TooHeavyBot?start=your_code_goes_here
--- main.py
+++ main.py
@@ -1,14 +1,34 @@
+from aiogram import Bot, Dispatcher, executor, types
 from src.util.config import Config
-from aiotg import Bot, Chat
+import logging
 
 
-bot = Bot(api_token=Config().get('telegram.token'))
+debug = Config().is_debug
+level = logging.INFO if debug else logging.ERROR
+logging.basicConfig(level=level)
+
+token = Config().get('telegram.token')
+bot = Bot(token)
+dp = Dispatcher(bot)
 
 
-@bot.command(r"hi")
-async def test_hi(chat: Chat, match):
-    await chat.send_text('hi!')
+START_PREFIX = '/start '
 
 
-if __name__ == "__main__":
-    bot.run(debug=Config().is_debug)
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    text = str(message.text)
+    if text.startswith(START_PREFIX):
+        code = text[len(START_PREFIX):]
+        await message.reply(f"Hi!\nYour code is {code}")
+    else:
+        await message.reply(f'Hi! this is start without params!')
+
+
+@dp.message_handler()
+async def echo(message: types.Message):
+    await message.reply(message.text, reply=False)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
