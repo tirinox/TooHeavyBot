138edc2d3a6b6920380ee5129f079aae77ab4f4b
Maksim Koltsov <tirinox@gmail.com>
2019-10-29 05:22:39 +0300

    async input; bootstrap input phase for usability

--- src/main_on_inputs.py
+++ src/main_on_inputs.py
@@ -8,9 +8,7 @@ import asyncio
 from aiogram.types import Message
 from tasks import task_manager
 from tasks.delete_profile import delete_profile
-from models.timeseries import TimePoint
-from tasks.weight_control import WEIGHT
-
+import threading
 
 USER_ID = 102
 
@@ -19,14 +17,6 @@ last_keyboard_anwer_map = {}
 
 async def testus():
     pass
-    # their_now = now_local_dt() - timedelta(days=1)
-    # tp = TimePoint(WEIGHT, USER_ID, their_now)
-    # tp.value = {
-    #     'weight': 102,
-    #     'percent': 35,
-    #     'ts': int(their_now.timestamp())
-    # }
-    # await tp.save()
 
 
 class FakeMessage(Message):
@@ -39,7 +29,6 @@ class FakeMessage(Message):
         global last_keyboard_anwer_map
         last_keyboard_anwer_map = {}
 
-        print('<', text)
         if isinstance(reply_markup, ReplyKeyboardRemove):
             print('[keyboard removed]')
         elif isinstance(reply_markup, ReplyKeyboardMarkup):
@@ -52,35 +41,53 @@ class FakeMessage(Message):
         return self
 
 
+async def async_input(prompt):
+    loop = asyncio.get_event_loop()
+    fut = loop.create_future()
+
+    def _run():
+        line = input(prompt)
+        loop.call_soon_threadsafe(fut.set_result, line)
+
+    threading.Thread(target=_run, daemon=True).start()
+    return await fut
+
+
+async def process_text_message(text_message):
+    if text_message in ['/quit', '/exit']:
+        return False
+    elif text_message == '/reset':
+        await Profile(USER_ID).set_dialog_state(None)
+    elif text_message.startswith('/dbs'):
+        await print_database(text_message[4:].strip())
+    elif text_message.startswith('/kill'):
+        user_id = int(text_message[5:].strip())
+        await delete_profile(user_id)
+    else:
+        # translate number to actual item text
+        if text_message in last_keyboard_anwer_map:
+            text_message = last_keyboard_anwer_map[text_message]
+            print(f'({text_message})')
+
+        message = FakeMessage(text=text_message)
+        from_user = MagicMock()
+        from_user.id = USER_ID
+        message.from_user = from_user
+
+        await message_handler.handle(message)
+    return True
+
+
 async def repl_loop(message_handler: MessageHandler):
     global last_keyboard_anwer_map
 
     await testus()
 
+    await process_text_message('-----BOOTSTRAP----')
     while True:
-        # i know that input() is sync, but for local testing it is totally OK!
-        text_message = input('> ').strip()
-        if text_message in ['/quit', '/exit']:
+        text_message = (await async_input('>')).strip()
+        if not await process_text_message(text_message):
             break
-        elif text_message == '/reset':
-            await Profile(USER_ID).set_dialog_state(None)
-        elif text_message.startswith('/dbs'):
-            await print_database(text_message[4:].strip())
-        elif text_message.startswith('/kill'):
-            user_id = int(text_message[5:].strip())
-            await delete_profile(user_id)
-        else:
-            # translate number to actual item text
-            if text_message in last_keyboard_anwer_map:
-                text_message = last_keyboard_anwer_map[text_message]
-                print(f'({text_message})')
-
-            message = FakeMessage(text=text_message)
-            from_user = MagicMock()
-            from_user.id = USER_ID
-            message.from_user = from_user
-
-            await message_handler.handle(message)
 
     print('Buy!')
 
